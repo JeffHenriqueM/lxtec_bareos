b4197193a0488757b50798bdbd2ac8866db112a0
JeffHenriqueM <jeffhenrique@outlook.com>
2023-12-07 17:22:27 -0400

    Discautol e Gráfica pex

--- 9oficio.cy.js
+++ 9oficio.cy.js
@@ -1,4 +1,4 @@
-describe('My First Test', () => {
+describe('9Oficio', () => {
   var dados = []
   var bkpFull = {}
 
--- bareos.cy.js
+++ bareos.cy.js
@@ -77,7 +77,7 @@ describe('9Oficio', () => {
     var dataAtual = new Date()
     dataAtual = dataAtual.toLocaleDateString().replaceAll('/', '_')
 
-    cy.writeFile('cypress/fixtures/' + '9oficio_' + dataAtual + '.json', dados)
+    cy.writeFile('cypress/fixtures/' + 'geral_' + dataAtual + '.json', dados)
 
   })
 
@@ -121,13 +121,12 @@ describe('9Oficio', () => {
           })
         } else {
           bkpFull.full = false
-          bkpFull.notes = "Backup full com erros"
+          bkpFull.notes = success
         }
       })
       dados.push(bkpFull)
     })
   }
-
 })
 
 describe('9Oficio_contabo', () => {
@@ -255,7 +254,7 @@ describe('9Oficio_contabo', () => {
           })
         } else {
           bkpFull.full = false
-          bkpFull.notes = "Backup full com erros"
+          bkpFull.notes = success
         }
       })
       dados.push(bkpFull)
@@ -388,7 +387,7 @@ describe('ADAMES', () => {
           })
         } else {
           bkpFull.full = false
-          bkpFull.notes = "Backup full com erros"
+          bkpFull.notes = success
         }
       })
       dados.push(bkpFull)
@@ -504,7 +503,284 @@ describe('AgricolaPanorama', () => {
           })
         } else {
           bkpFull.full = false
-          bkpFull.notes = "Backup full com erros"
+          bkpFull.notes = success
+        }
+      })
+      dados.push(bkpFull)
+    })
+  }
+})
+
+describe('Discautol', () => {
+  var dados = []
+  var bkpFull = {}
+
+  it('job_srvcrdts01', () => {
+    cy.visit('http://179.185.91.250:53080/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://179.185.91.250:53080/bareos-webui/job/index?jobname=job_srvcrdts01')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.wait(500)
+
+    pegarDados('job_srvcrdts01')
+
+  })
+  it('job_srvcrdts02', () => {
+    cy.visit('http://179.185.91.250:53080/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://179.185.91.250:53080/bareos-webui/job/index?jobname=job_srvcrdts02')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.wait(500)
+
+    pegarDados('job_srvcrdts02')
+
+  })
+
+  it('job_srvcrdts03', () => {
+    cy.visit('http://179.185.91.250:53080/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://179.185.91.250:53080/bareos-webui/job/index?jobname=job_srvcrdts03')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.wait(500)
+
+    pegarDados('job_srvcrdts03')
+  })
+  
+  it('job_sfad0102', () => {
+    cy.visit('http://179.185.91.250:53080/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://179.185.91.250:53080/bareos-webui/job/index?jobname=job_sfad0102')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.wait(500)
+
+    pegarDados('job_sfad0102')
+
+  })
+
+  it('job_srvcrdcon01', () => {
+    cy.visit('http://179.185.91.250:53080/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://179.185.91.250:53080/bareos-webui/job/index?jobname=job_srvcrdcon01')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.wait(500)
+
+    pegarDados('job_srvcrdcon01')
+
+  })
+
+
+  it('BackupCatalog', () => {
+    cy.visit('http://179.185.91.250:53080/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://179.185.91.250:53080/bareos-webui/job/index?jobname=BackupCatalog')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.wait(500)
+
+    pegarDados('BackupCatalog')
+
+  })
+
+
+  it('backup-bareos-fd', () => {
+    cy.visit('http://179.185.91.250:53080/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://179.185.91.250:53080/bareos-webui/job/index?jobname=backup-bareos-fd')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.wait(500)
+
+    pegarDados('backup-bareos-fd')
+    var dataAtual = new Date()
+    dataAtual = dataAtual.toLocaleDateString().replaceAll('/', '_')
+
+    cy.writeFile('cypress/fixtures/' + 'Discautol_' + dataAtual + '.json', dados)
+  })
+
+  function pegarDados(name) {
+    bkpFull = {}
+    bkpFull.title = 'Discautol_' + name
+
+    //Pegar dados último Full
+    cy.get('[data-index="0"] > :nth-child(8)').then(function (e) {
+      const dataEnd = e.text()
+      cy.get('[data-index="0"] > :nth-child(13)').then(function (e) {
+        const success = e.text()
+        if (success == "Success") {
+          //Método responsável por verificar se não há nenhum erro
+          cy.get('[data-index="0"] > :nth-child(12)').then(function (e) {
+            const erros = Number(e.text())
+            bkpFull.errosFull = e.text();
+            if (erros == 0) {
+              cy.get('[data-index="0"] > :nth-child(2)').then(function (e) {
+                const id = e.text()
+                bkpFull.id = id
+              })
+              cy.get('[data-index="0"] > :nth-child(11)').then(function (e) {
+                const tamanhoArquivo = e.text()
+                bkpFull.tamanho = tamanhoArquivo
+              })
+              cy.get('[data-index="0"] > :nth-child(7)').then(function (e) {
+                const dataInicio = e.text()
+                bkpFull.dataInicio = dataInicio
+              })
+              cy.get('[data-index="0"] > :nth-child(8)').then(function (e) {
+                const dataFim = e.text()
+                bkpFull.dataFim = dataFim
+              })
+              bkpFull.full = true
+              bkpFull.notes = "OK"
+            } else {
+              bkpFull.full = false
+              bkpFull.notes = "Erros : " + erros
+            }
+          })
+        } else {
+          bkpFull.full = false
+          bkpFull.notes = success
+        }
+      })
+      dados.push(bkpFull)
+    })
+  }
+})
+
+describe('GraficaPex-Bancos', () => {
+  var dados = []
+  var bkpFull = {}
+
+  it('job_pexonline', () => {
+    cy.visit('http://177.174.43.235:53081/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://177.174.43.235:53081/bareos-webui/job/index?jobname=job_pexonline')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.wait(500)
+
+    pegarDados('job_pexonline')
+
+  })
+
+  it('job_srvdb02', () => {
+    cy.visit('http://177.174.43.235:53081/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://177.174.43.235:53081/bareos-webui/job/index?jobname=job_srvdb02')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.get('[style="left: 36%;"]').click()
+    cy.wait(500)
+
+    pegarDados('job_srvdb02')
+
+  })
+
+  it('BackupCatalog', () => {
+    cy.visit('http://177.174.43.235:53081/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://177.174.43.235:53081/bareos-webui/job/index?jobname=BackupCatalog')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.wait(500)
+
+    pegarDados('BackupCatalog')
+
+  })
+
+
+  it('backup-bareos-fd', () => {
+    cy.visit('http://177.174.43.235:53081/bareos-webui/')
+    cy.get('input[name="consolename"]').type('admin')
+    cy.get('input[name="password"]').type('ViQUingue')
+    cy.contains('Login').click()
+
+    cy.visit('http://177.174.43.235:53081/bareos-webui/job/index?jobname=backup-bareos-fd')
+
+    cy.get('[placeholder="Level"]').type('full')
+    cy.wait(500)
+
+    pegarDados('backup-bareos-fd')
+    var dataAtual = new Date()
+    dataAtual = dataAtual.toLocaleDateString().replaceAll('/', '_')
+
+    cy.writeFile('cypress/fixtures/' + 'GraficaPex-Bancos_' + dataAtual + '.json', dados)
+  })
+
+  function pegarDados(name) {
+    bkpFull = {}
+    bkpFull.title = 'GraficaPex-Bancos_' + name
+
+    //Pegar dados último Full
+    cy.get('[data-index="0"] > :nth-child(8)').then(function (e) {
+      const dataEnd = e.text()
+      cy.get('[data-index="0"] > :nth-child(13)').then(function (e) {
+        const success = e.text()
+        if (success == "Success") {
+          //Método responsável por verificar se não há nenhum erro
+          cy.get('[data-index="0"] > :nth-child(12)').then(function (e) {
+            const erros = Number(e.text())
+            bkpFull.errosFull = e.text();
+            if (erros == 0) {
+              cy.get('[data-index="0"] > :nth-child(2)').then(function (e) {
+                const id = e.text()
+                bkpFull.id = id
+              })
+              cy.get('[data-index="0"] > :nth-child(11)').then(function (e) {
+                const tamanhoArquivo = e.text()
+                bkpFull.tamanho = tamanhoArquivo
+              })
+              cy.get('[data-index="0"] > :nth-child(7)').then(function (e) {
+                const dataInicio = e.text()
+                bkpFull.dataInicio = dataInicio
+              })
+              cy.get('[data-index="0"] > :nth-child(8)').then(function (e) {
+                const dataFim = e.text()
+                bkpFull.dataFim = dataFim
+              })
+              bkpFull.full = true
+              bkpFull.notes = "OK"
+            } else {
+              bkpFull.full = false
+              bkpFull.notes = "Erros : " + erros
+            }
+          })
+        } else {
+          bkpFull.full = false
+          bkpFull.notes = success
         }
       })
       dados.push(bkpFull)
